b5591b2c73a517933b930243d907866d695d9c56
oumaima <oumaimanaanaapro@gmail.com>
2024-01-29 14:26:03 +0100

    to branch ouma

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -1,7 +1,7 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_t - Creates a binary tree node
+ * binary_tree_node - Creates a binary tree node
  *
  * @parent: A pointer to the parent node of the node to create
  *
