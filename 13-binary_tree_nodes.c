947808918193f3cfc175e9b1bf0a5f5a445e28e2
Badr Annabi <sofiaannabi@sofias-MacBook-Pro.local>
2024-01-30 16:11:28 +0100

    TASK 13

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_nodes - counts the nodes with at least 1 child in a binary tree.
+ * @tree: ponter to the root node of the tree.
+ * Return: number of nodes with at least 1 child in a binary tree.
+*/
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    if (!tree || (!(tree->left) && !(tree->right)))
+        return(0);
+    return ( 1 + binary_tree_nodes(tree->right) + binary_tree_nodes(tree->left));
+}
