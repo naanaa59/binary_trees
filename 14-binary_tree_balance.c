e2af3342af0dec4bb840277a7c25242af2016e33
oumaima <oumaimanaanaapro@gmail.com>
2024-01-31 21:53:33 +0100

    betty fixed for 5 and 14

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -32,7 +32,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int bal_factor = 0, l_height = -1 , r_height = -1;
+	int bal_factor = 0, l_height = -1, r_height = -1;
 
 	if (tree == NULL)
 		return (0);
