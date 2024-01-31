1700f567f6a6878b03eb5b377a3a0253d1ccdac1
Badr Annabi <sofiaannabi@sofias-MacBook-Pro.local>
2024-01-31 21:53:45 +0100

    TASK 0

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -4,6 +4,7 @@
  * binary_tree_node - creates a binary tree node.
  * @parent: pointer to the parent node of the node to create.
  * @value: the value to put in the new node.
+ * Return: the node.
 */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
