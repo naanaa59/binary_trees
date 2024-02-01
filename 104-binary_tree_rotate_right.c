3912cf8f9d847eb1f9bff607f67123551c452e75
oumaima <oumaimanaanaapro@gmail.com>
2024-02-01 21:28:44 +0100

    new_root->parent = NULL

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -17,7 +17,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	if (tree->left->right)
 		tree->left->right->parent = tree;
 
-	new_root = NULL;
+	new_root->parent = NULL;
 	new_root = tree->left;
 	tree->left = new_root->right;
 	new_root->right = tree;
