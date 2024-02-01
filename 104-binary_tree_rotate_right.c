04fb64735aaa3328e019ca2d26e9f8b58cccf42b
oumaima <oumaimanaanaapro@gmail.com>
2024-02-01 21:08:07 +0100

    104 3 output ok

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -17,6 +17,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 	new_root = tree->left;
 	tree->left = new_root->right;
 	new_root->right = tree;
+	tree->parent = new_root;
 
 	return (new_root);
 }
